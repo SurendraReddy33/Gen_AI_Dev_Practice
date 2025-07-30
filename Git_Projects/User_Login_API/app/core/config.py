9ab654701694d28b26fb45f92baeda5792f22668
Surendra Reddy Gandra <surendrareddy.gandra@thezennialpro.com>
2025-07-30 17:28:45 +0530

    Resolved Secret key issue

--- Git_Projects/User_Login_API/app/core/config.py
+++ Git_Projects/User_Login_API/app/core/config.py
@@ -1,7 +1,7 @@
 import secrets 
  
 # Use a fixed secret key for JWT
-SECRET_KEY = secrets.token_hex(32)  # fallback if .env missing
+SECRET_KEY = secrets.token_hex(32) 
  
 # MongoDB URI
 MONGO_URI = "mongodb+srv://surendrareddygandra3:onTwB6qFMES4HF24@genai-cluster.cb98ycb.mongodb.net/"
