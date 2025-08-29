5144daf37ceb48d52de261f34023d4d9d601ddac
SurendraReddy33 <surendrareddy.gandra@thezennialpro.com>
2025-08-29 18:55:26 +0530

    Delete Git_Projects/User_Login_API directory

--- Git_Projects/User_Login_API/app/core/config.py
+++
@@ -1,9 +0,0 @@
-import secrets 
- 
-# Use a fixed secret key for JWT
-SECRET_KEY = secrets.token_hex(32) 
-
-# MongoDB URI
-MONGO_URI = "mongodb+srv://surendrareddygandra3:onTwB6qFMES4HF24@genai-cluster.cb98ycb.mongodb.net/"
- 
-
